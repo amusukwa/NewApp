e4f175fd1d24d356491ecc2240d614d7d8555f27
alice <amusukwa@gmail.com>
2022-07-13 20:31:25 +0100

    editing Pic  activity

--- app/src/main/java/com/example/newapp/PicActivity.kt
+++ app/src/main/java/com/example/newapp/PicActivity.kt
@@ -1,13 +1,20 @@
 package com.example.newapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PicActivity : AppCompatActivity() {
+    lateinit var Imageuri: URI
+   // private lateinit var storageReference: FirebaseStorage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pic)
@@ -18,13 +25,21 @@ class PicActivity : AppCompatActivity() {
 
     }
 
-     fun addPic() {
-        val  intent = Intent(this,
-            ListActivity::class.java)
-        startActivity(intent)
-        Toast.makeText(this,"List Selected", Toast.LENGTH_SHORT).show()
+    val  getContent = registerForActivityResult(ActivityResultContracts
+        .GetContent()){uri: Uri?-> uri?.let { imageUri-> }}
+
+
+    fun addPics(view: View) {
+        getContent.launch("image/*")
     }
 
-    fun upLoad(view: View) {}
-    fun addPics(view: View) {}
+
+    fun upLoad(view: View) {
+
+        val formatter = SimpleDateFormat("yyyy_mo_dd__min", Locale.ENGLISH)
+        val now = Date()
+        val fileName = formatter.format(now)
+       // val storageReference = FirebaseStorage
+
+    }
 }
